ad0631211380526264ae0fb7f159fc7909641e8a
phungson185 <phungson185@gmail.com>
2022-02-18 21:38:08 +0700

    fix: bug

--- src/init/init.c
+++ src/init/init.c
@@ -7,15 +7,11 @@
 int main()
 {
     btinit();
-    BTA *dict;
     BTA *note;
     BTA *question;
-
-    dict = btcrt("../db/dict.bt", 0, 0);
     note = btcrt("../db/note.bt", 0, 0);
     question = btcrt("../db/question.bt", 0, 0);
     
     btcls(note);
     btcls(question);
-    btcls(dict);
 }
